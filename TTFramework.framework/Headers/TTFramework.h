2c2cdc62d3986c16f498d496c96afb147c1471ac
xuzepei <xuzepei@smalltreemedia.com>
2018-07-11 12:12:21 +0800

    update.

---
+++ TTFramework.framework/Headers/TTFramework.h
@@ -0,0 +1,21 @@
+//
+//  TTFramework.h
+//  TTFramework
+//
+//  Created by xuzepei on 2018/7/11.
+//  Copyright © 2018年 xuzepei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TTFramework.
+FOUNDATION_EXPORT double TTFrameworkVersionNumber;
+
+//! Project version string for TTFramework.
+FOUNDATION_EXPORT const unsigned char TTFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TTFramework/PublicHeader.h>
+
+#import "TTLog.h"
+
+
---
+++ TTFramework.framework/Headers/TTLog.h
@@ -0,0 +1,15 @@
+//
+//  TTLog.h
+//  TTFramework
+//
+//  Created by xuzepei on 2018/7/11.
+//  Copyright © 2018年 xuzepei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TTLog : NSObject
+
+- (void)log;
+
+@end
